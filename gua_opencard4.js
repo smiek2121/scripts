e806ec4706d50b165875a192bd1aeaedf69f9ed4
smiek2221 <86996043+smiek2221@users.noreply.github.com>
2021-08-04 16:20:31 +0800

    修改我的奖品显示

--- gua_opencard4.js
+++ gua_opencard4.js
@@ -70,7 +70,6 @@ message = ""
   }
   $.shareUuid = '8eef88dbbb5e4a11b04f222b78b195c8'
   console.log(`入口:\nhttps://lzdz1-isv.isvjcloud.com/dingzhi/dz/openCard/activity/832865?activityId=c225ad5922cf4ac8b4a68fd37f486088&shareUuid=${$.shareUuid}`)
-  // 134
   for (let i = 0; i < cookiesArr.length; i++) {
     cookie = cookiesArr[i];
     if (cookie) {
@@ -182,13 +181,14 @@ function getDrawRecordHasCoupon() {
         } else {
           // console.log(data)
           data = JSON.parse(data)
-          console.log(`我的奖品`)
+          console.log(`我的奖品：`)
+          let num = 0
           for(let i in data.data){
-            if(i>=15) console.log(`......`)
-            if(i>=15) break
             let item = data.data[i]
-            console.log(`${item.infoType != 10 && item.value +':' || ''}${item.infoName}`)
+            if(item.value == '邀请好友') num++;
+            if(item.value != '邀请好友') console.log(`${item.infoType != 10 && item.value +':' || ''}${item.infoName}`)
           }
+          if(num > 0) console.log(`邀请好友(${num}):${num*20}京豆`)
         }
       } catch (e) {
         $.logErr(e, resp)
