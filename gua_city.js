480fc2d5d0179de4f6edf9188bad79db52170b1c
smiek2221 <86996043+smiek2221@users.noreply.github.com>
2021-10-20 03:00:35 +0800

    城城领现金

--- gua_city.js
+++ gua_city.js
@@ -3,7 +3,7 @@
 =================================Quantumultx=========================
 [task_local]
 #城城领现金
-0 0-23/5,22 * * * gua_city.js, tag=城城领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+0 0-23/5,22 10 * * gua_city.js, tag=城城领现金, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
  */
 const $ = new Env('城城领现金');
