34195e332d25a6b6f80d9b0d28cbd24df8738849
smiek2221 <smiek2221@126.com>
2021-11-02 01:38:55 +0800

    修改定时

--- gua_redEnvelope.js
+++ gua_redEnvelope.js
@@ -4,7 +4,7 @@ ck1助力 作者
 其余助力ck1
 https://u.jd.com/3C7eCOr
 跳转到app 可查看助力情况
-cron 1 0,12,18 * * * gua_RedEnvelope.js
+1 0,12,18 * * * https://raw.githubusercontent.com/smiek2121/scripts/master/gua_RedEnvelope.js
 
 返利变量：gua_redEnvelope_rebateCode，默认给脚本作者返利，若需要返利给自己，请自己修改返利变量gua_redEnvelope_rebateCode
 例：gua_redEnvelope_rebateCode="你的返利code"
