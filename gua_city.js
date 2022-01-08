5ca33181772228f3973dfb3217bcb95119ea7c49
smiek2121 <smiek2221@126.com>
2022-01-09 01:58:27 +0800

    .

--- gua_city.js
+++ gua_city.js
@@ -31,7 +31,6 @@ if ($.isNode()) {
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let inviteCodes = [
   '-ryUX_gJZGBBYBWRT9zE8Q1IR0FJxuYS',
-  '-ryULMRsPj4EB1WkGaGtuzDU_gdn6tw'
 ]
 $.shareCodesArr = [];
 
